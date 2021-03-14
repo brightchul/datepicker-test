7d1067478aac2d0d0533f6669be03d156e91cf2a
brightchul <abhbtbb1@gmail.com>
2021-03-14 20:42:51 +0900

    feat: apply Dropdown componet to TestPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import styled from "@emotion/styled";
 import React from "react";
-import { MyText } from "./component/myText";
-
-const Test1 = styled.div`
-  border: 1px solid black;
-  background-color: #eee;
-`;
-
-const Test2 = styled.div`
-  background-color: ${({ theme }) => theme.color["black-12"]};
-`;
+import TestPage from "./TestPage";
 
 function App() {
   return (
-    <div className="App">
-      <Test1>
-        <MyText myFont="bold-32" myColor="dodger-blue-008">
-          ㅌㅔ스트1
-        </MyText>
-      </Test1>
-      <Test2>테스트2</Test2>
+    <div>
+      <TestPage></TestPage>
     </div>
   );
 }
---
+++ src/TestPage.tsx
@@ -0,0 +1,115 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { MyText } from "./component/myText";
+import { Dropdown } from "./component/dropdown";
+import { DatePicker } from "./component";
+
+const TestPage: React.FC = () => {
+  return (
+    <BackgroundWrapper>
+      <SelectTimeAreaWrapper>
+        <SelectTimeAreaTitle>
+          <MyText myFont="bold-32" myColor="gray-20">
+            응시 기간 설정
+          </MyText>
+        </SelectTimeAreaTitle>
+        <SelectTimeAreaContent>
+          <StartTimeWrapper>
+            <StartTimeTitle>
+              <MyText myFont="medium-14">응시 시작일</MyText>
+            </StartTimeTitle>
+            <StartTimeSelectArea>
+              <DatePicker width="236px"></DatePicker>
+              <Dropdown
+                width="152px"
+                data={Array(24)
+                  .fill(0)
+                  .map(
+                    (_, idx) =>
+                      `${idx < 12 ? "오전" : "오후"} ${
+                        idx % 12 === 0 ? 12 : idx % 12
+                      }시`
+                  )}
+              />
+              <Dropdown
+                width="152px"
+                data={["0분", "10분", "20분", "30분", "40분", "50분"]}
+              />
+            </StartTimeSelectArea>
+          </StartTimeWrapper>
+          <Divider />
+
+          <StartTimeWrapper>
+            <StartTimeTitle>
+              <MyText myFont="medium-14">응시 마감일</MyText>
+            </StartTimeTitle>
+            <StartTimeSelectArea>
+              <DatePicker width="236px"></DatePicker>
+              <Dropdown
+                width="152px"
+                data={Array(24)
+                  .fill(0)
+                  .map(
+                    (_, idx) =>
+                      `${idx < 12 ? "오전" : "오후"} ${
+                        idx % 12 === 0 ? 12 : idx % 12
+                      }시`
+                  )}
+              />
+              <Dropdown
+                width="152px"
+                data={["0분", "10분", "20분", "30분", "40분", "50분"]}
+              />
+            </StartTimeSelectArea>
+          </StartTimeWrapper>
+        </SelectTimeAreaContent>
+      </SelectTimeAreaWrapper>
+    </BackgroundWrapper>
+  );
+};
+
+const Divider = styled.hr`
+  width: 100%;
+  border: 1px solid ${({ theme }) => theme.color.silver};
+`;
+
+const SelectTimeAreaContent = styled.div`
+  margin-top: 56px;
+  display: grid;
+  row-gap: 32px;
+`;
+
+const StartTimeTitle = styled.div``;
+const StartTimeWrapper = styled.div`
+  display: grid;
+  row-gap: 8px;
+`;
+
+const StartTimeSelectArea = styled.div`
+  display: flex;
+  column-gap: 16px;
+`;
+
+const SelectTimeAreaTitle = styled.div`
+  width: 100%;
+`;
+
+const BackgroundWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.color["eggplant-80"]};
+`;
+
+const SelectTimeAreaWrapper = styled.div`
+  width: 824px;
+  height: 485px;
+  padding: 40px 84px;
+  margin: 25vh auto;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.color["white"]};
+`;
+
+export { TestPage };
+export default TestPage;
